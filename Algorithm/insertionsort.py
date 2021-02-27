aaac49e2d056ba33351215fa1c494ad5aafd3a68
bejpark <bjpark0506@naver.com>
2021-02-27 17:47:54 +0900

    2.27 sort

---
+++ Algorithm/insertionsort.py
@@ -0,0 +1,12 @@
+def insert_sort(data):
+    for index in range(len(data)-1):
+        for index2 in range(index+1,0,-1):
+            if data[index2]<data[index2-1]:
+                data[index2],data[index2-1] = data[index2-1],data[index2]
+            else:
+                break
+    return data
+
+import random
+data_list=random.sample(range(100),50)
+print(insert_sort(data_list))
